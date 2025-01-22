19db5392a3a7dd35f265dbaa4a5ee0ae1eab4fc5
anonymousInterview <32-fried.swivels@icloud.com>
2025-01-22 08:10:42 -0800

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Ulises Giacoman on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
